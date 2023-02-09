35fd2cfcb98cb92e359c5a13312f132a07a126b9
ethanbaruh <ethanbaruh>
2023-02-08 22:14:13 -0800

    add finish to location save

--- app/src/main/java/com/example/cse110_team2/InputLocation.java
+++ app/src/main/java/com/example/cse110_team2/InputLocation.java
@@ -3,6 +3,7 @@ package com.example.cse110_team2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class InputLocation extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class InputLocation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_location);
+    }
+
+    public void onLocationSaveBtnClicked(View view) {
+        finish();
     }
 }
