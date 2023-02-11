f3a640ede325404669f2873fcedf60be1c6729c4
DX <danielxu.314@gmail.com>
2023-02-11 11:10:43 -0800

    Revert "create location class and location var that stores current GPS location"
    This reverts commit 742241d8eb6183b0fe843ae93f27fa1fc8615d1f.

--- app/src/main/java/com/example/cse110_team2/MainActivity.java
+++ app/src/main/java/com/example/cse110_team2/MainActivity.java
@@ -1,49 +1,17 @@
 package com.example.cse110_team2;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private LocationManager locationManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //TextView lon= findViewById(R.id.Lon);
-        //TextView lat= findViewById(R.id.Lat);
-
-        MyLocation myloc = new MyLocation(0,0);
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED &&
-        ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION},1);
-        }
-
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 1, new LocationListener() {
-
-            @Override
-            public void onLocationChanged(@NonNull Location location) {
-                myloc.setLon(location.getLongitude());
-                myloc.setLat(location.getLatitude());
-                //lon.setText("Longitude: " + String.valueOf(myloc.getLon()));
-                //lat.setText("Latitude: " + String.valueOf(myloc.getLat()));
-            }
-        });
     }
 
     public void onNewLocationBtnClicked(View view) {
