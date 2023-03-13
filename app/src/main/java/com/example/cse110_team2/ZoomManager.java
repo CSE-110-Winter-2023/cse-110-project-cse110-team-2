08b9a2eae4b699484b9aec8815fd8c8a6d98938c
SiddhantKumar-Dev <97648275+siddhant-kumar-ucsd@users.noreply.github.com>
2023-03-13 13:56:17 -0700

    Implement Distance Based Zoom

--- app/src/main/java/com/example/cse110_team2/ZoomManager.java
+++ app/src/main/java/com/example/cse110_team2/ZoomManager.java
@@ -1,17 +1,48 @@
 package com.example.cse110_team2;
 import android.view.View;
 
+import java.util.Arrays;
+
 public class ZoomManager {
     int zoomAmount;
     int[] zoomLimit;
+    double[] relativePixelWidth;
+    double compassWidth;
 
     public ZoomManager(){
-        zoomAmount = 0;
-        zoomLimit = new int[]{1, 10, 500, 50000};
+        this.zoomAmount = 0;
+        this.zoomLimit = new int[]{1, 10, 500, 50000};
+        this.relativePixelWidth = new double[]{200, 100, 66.66, 50};
+    }
+
+    public void setCompassWidth(double compassWidth) {
+        this.compassWidth = compassWidth;
     }
 
     public int getZoomAmount(){
         return zoomAmount;
+    }
+
+    public double getCompassWidth(){
+        return this.compassWidth;
+    }
+    private double getCompassWidthRatio(){
+        return compassWidth/400;
+    }
+
+    public double getRadius(double distance){
+        int[] zoomSub = Arrays.copyOfRange(this.zoomLimit,0, zoomAmount + 1);
+        for(int i = 0; i<zoomSub.length; i += 1){
+            if(distance < (double) zoomSub[i]){
+                double dist_ratio = distance / (double) zoomSub[i];
+                double pixel_val_ring = this.relativePixelWidth[this.zoomAmount];
+                double lower_bound = pixel_val_ring * i;
+                double upper_bound = pixel_val_ring * (i+1);
+                return (((upper_bound - lower_bound) * dist_ratio) + lower_bound) * this.getCompassWidthRatio();
+
+            }
+        }
+        return 0.00;
     }
 
     public void zoomOut(){
@@ -29,7 +60,8 @@ public class ZoomManager {
         return zoomAmount != 0;
 
     }
-    public int getMaxZoom(){
+
+    public int get_curr_zoom_max(){
         return zoomLimit[zoomAmount];
     }
 
