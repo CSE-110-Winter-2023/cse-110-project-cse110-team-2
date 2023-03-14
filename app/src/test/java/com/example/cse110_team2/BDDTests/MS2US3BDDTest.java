16a9091715d644638d8e45b2091f1954ab54b96f
SiddhantKumar-Dev <97648275+siddhant-kumar-ucsd@users.noreply.github.com>
2023-03-14 12:04:16 -0700

    Update MS2US3BDDTest.java

--- app/src/test/java/com/example/cse110_team2/BDDTests/MS2US3BDDTest.java
+++ app/src/test/java/com/example/cse110_team2/BDDTests/MS2US3BDDTest.java
@@ -66,7 +66,7 @@ public class MS2US3BDDTest {
             ConstraintLayout.LayoutParams friend2Layout = (ConstraintLayout.LayoutParams) friend2Text.getLayoutParams();
 
             assertEquals(friend1Layout.leftMargin,144);
-            assertEquals(friend1Layout.topMargin,136);
+            assertEquals(friend1Layout.topMargin,144);
             assertEquals(friend2Layout.leftMargin,136);
             assertEquals(friend2Layout.topMargin,136);
             activity.setMock(false);
